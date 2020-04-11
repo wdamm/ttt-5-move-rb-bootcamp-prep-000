2b18b2cd97f18523a5239e1918134ed5094ce374
William Damm <wdamm@powerhrg.com>
2020-04-11 17:16:00 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,10 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(number)
- input = number.to_i
-return input - 1
-end
 
 
 def move(array, index, charachter = "X")
